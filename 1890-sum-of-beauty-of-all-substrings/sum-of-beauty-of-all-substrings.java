8284dfb8a06fdc6af0c74b20aafc91ce382ac218
shivank7269 <78946847+shivank7269@users.noreply.github.com>
2025-04-17 11:06:29 +0530

    Time: 43 ms (99.35%) | Memory: 42.7 MB (76.30%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,23 +1,27 @@
 class Solution {
     public int beautySum(String s) {
-        int sum = 0;
+        char[] arr = s.toCharArray();
+        int res = 0;
+
         for (int i = 0; i < s.length(); i++) {
-            HashMap<Character, Integer> map = new HashMap<>();
+            int[] count = new int[26];
+            int max = 0;
+
             for (int j = i; j < s.length(); j++) {
-                char c = s.charAt(j);
-                map.put(c, map.getOrDefault(c, 0) + 1);
-                sum += calculateBeauty(map);
+                count[arr[j] - 'a']++;
+                int min = Integer.MAX_VALUE;
+                max = Math.max(max, count[arr[j] - 'a']);
+
+                for (int k = 0; k < 26; k++) {
+                    if (count[k] > 0) {
+                        min = Math.min(min, count[k]);
+                    }
+                }
+
+                res += (max - min);
             }
         }
-        return sum;
-    }
 
-    public int calculateBeauty(HashMap<Character, Integer> map) {
-        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-        for (int count : map.values()) {
-            if (count > max) max = count;
-            if (count < min) min = count;
-        }
-        return max - min;
+        return res;
     }
 }
