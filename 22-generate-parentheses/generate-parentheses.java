59b88c8e1c566d17d70691721d2a6c193800ec4a
shivank7269 <78946847+shivank7269@users.noreply.github.com>
2025-05-28 10:15:05 +0530

    Time: 2 ms (46.53%) | Memory: 43.1 MB (68.87%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -7,22 +7,18 @@ class Solution {
     }
 
     public void backtrack(int n, int leftCount, int rightCount, List<String> output, List<String> result) {
-        // Base case where count of left and right braces is "n"
+       
         if (leftCount >= n && rightCount >= n) {
-            // Join the array elements into a string without any separators.
+            
             String outputStr = String.join("", output);
             result.add(outputStr);
         }
-
-        // Case where we can still append left braces
         if (leftCount < n) {
             output.add("(");
             backtrack(n, leftCount + 1, rightCount, output, result);
             output.remove(output.size() - 1);
         }
 
-        // Case where we append right braces if the current count of right braces is
-        // less than the count of left braces
         if (rightCount < leftCount) {
             output.add(")");
             backtrack(n, leftCount, rightCount + 1, output, result);
