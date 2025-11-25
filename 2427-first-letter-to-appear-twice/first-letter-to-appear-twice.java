a14f1b60f2ee399d552c928b7a7660f2ced13ab9
Shivank singh chandel <78946847+shivank7269@users.noreply.github.com>
2025-11-26 00:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (7.91%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,7 +1,16 @@
 class Solution {
     public char repeatedCharacter(String s) {
         for(int i=1;i<s.length();i++){
-            for(int j=0;j<i;j++){
+            /*
+            "abccbaacz"
+            i=1 b 
+                j=0 b==a ? false
+            i=2 c
+                j=0 c==a ? false c==b ? false
+            i=3 c
+                j=0 c==a ? false c==b ? false c==c ? true return 
+             */ 
+             for(int j=0;j<i;j++){
                 if(s.charAt(i) == s.charAt(j)){
                     return s.charAt(i);
                 }
