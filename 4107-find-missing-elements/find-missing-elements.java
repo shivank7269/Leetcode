fee33c5fd4f36da755037b62d145766da622c654
Shivank singh chandel <78946847+shivank7269@users.noreply.github.com>
2025-12-07 00:15:01 +0530

    Time: 2 ms (94.29%) | Memory: 46.5 MB (92.52%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -1,29 +1,33 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        int min = 101 , max = 0 ;
-
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            if(nums[i] > max) max = nums[i];
-            if(nums[i] < min) min = nums[i];
-        }
-
-        Set<Integer> set = new HashSet<>();
-
-        for(int num : nums)
-        {
-            set.add(num);
-        }
-        List<Integer> res = new ArrayList<>();
-
-        for(int i = min+1 ; i < max ; i++)
-        {
-            if(!set.contains(i))
-            {
-               res.add(i);
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
             }
         }
 
-        return res;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+        }
+
+        for (int i = min; i <= max; i++) {
+            boolean isfound = false;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == i) {
+                    isfound = true;
+                    break;
+                }
+            }
+            if (!isfound) {
+                list.add(i);
+            }
+        }
+
+        return list;
     }
 }
