64de572cdc1a68e4ba2a38845922a223a50e7bf7
Shivank singh chandel <78946847+shivank7269@users.noreply.github.com>
2025-12-10 23:52:10 +0530

    Time: 2 ms (93.61%) | Memory: 47 MB (84.16%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -2,15 +2,13 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         int[] ans = new int[2];
         int[] freq = new int[nums.length+1];
-        for(int i=0;i<nums.length;i++){
-            freq[nums[i]]++;
+        for(int n : nums){
+            freq[n]++;
         }
-        System.out.println(Arrays.toString(freq));
-        for (int i = 1; i <= nums.length ; i++) {
+        for(int i=1;i<=nums.length;i++){
             if(freq[i] == 2) ans[0] = i;
-            else if (freq[i] == 0) ans[1] = i;
+            else if(freq[i] == 0) ans[1] = i;
         }
-        System.out.println(Arrays.toString(ans));
         return ans;
     }
 }
