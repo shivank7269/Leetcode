186bf83b6ac447cf0e7503c76aed434853a46cf7
Shivank singh chandel <78946847+shivank7269@users.noreply.github.com>
2025-12-11 23:26:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (40.86%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,20 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumOfOdd = 0;
-        int sumOfEven =0;
-        for(int i=1;i<=n;i++){
-            sumOfOdd+=2*i-1;
-            sumOfEven+=2*i;
-        }
-        
-        return gcd(sumOfOdd,sumOfEven);
-    }
-    public int gcd(int a , int b){
-        int result = Math.min(a,b);
-        while(result > 0){
-            if(a%result==0 && b%result==0) break;
-            result--;
-        }
-        return result;
+        return n;
     }
 }
