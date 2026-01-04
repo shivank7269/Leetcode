5ce0c8b53d15118c260a650f5276f0c026f47bc3
Shivank singh chandel <78946847+shivank7269@users.noreply.github.com>
2026-01-04 15:13:17 +0530

    Time: 1 ms (50.02%) | Memory: 44 MB (75.34%) - LeetSync

--- 4179-largest-even-number/largest-even-number.java
+++ 4179-largest-even-number/largest-even-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public String largestEven(String s) {
-        int l = s.lastIndexOf('2');
-        if(l<=-1) return "";
+        int l = s.lastIndexOf('2'); // 2 konse index pe hai akhri me
+        if(l==-1) return "";       
         return s.substring(0,l+1);
     }
 }
