4572ac48faba5d782e7a56b15614c0cb81dc2c71
Shivank singh chandel <78946847+shivank7269@users.noreply.github.com>
2026-01-10 18:39:02 +0530

    Time: 1 ms (99.94%) | Memory: 43 MB (27.45%) - LeetSync

--- 4147-minimum-cost-to-acquire-required-items/minimum-cost-to-acquire-required-items.java
+++ 4147-minimum-cost-to-acquire-required-items/minimum-cost-to-acquire-required-items.java
@@ -1,15 +1,8 @@
 class Solution {
     public long minimumCost(int cost1, int cost2, int costBoth, int need1, int need2) {
-        long individual = (long) need1 * cost1 + (long) need2 * cost2;
-        int common = Math.min(need1, need2);
-        long bothStrategy = 0;
-        long effectiveBoth = Math.min((long) costBoth, (long) cost1 + cost2);
-        bothStrategy += (long) common * effectiveBoth;
-        if (need1 > need2) {
-            bothStrategy += (long) (need1 - need2) * Math.min(cost1, costBoth);
-        } else {
-            bothStrategy += (long) (need2 - need1) * Math.min(cost2, costBoth);
-        }
-        return Math.min(individual, bothStrategy);
+        long ans = (long)cost1*need1 + (long)cost2*need2;
+        ans = Math.min(ans,(long)Math.max(need1,need2)*costBoth);
+        ans= Math.min(ans,(long)Math.min(need1,need2)*costBoth+((need1>=need2)?(long)(need1-need2)*cost1:(long)(need2-need1)*cost2));
+        return ans;
     }
 }
