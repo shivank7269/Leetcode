7e5e346df7a3ebeaf4f0735e50d6298cf8ef6a29
Shivank singh chandel <78946847+shivank7269@users.noreply.github.com>
2026-01-25 00:47:23 +0530

    Time: 1 ms (96.55%) | Memory: 42.1 MB (98.89%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,9 +1,12 @@
 class Solution {
     public int mySqrt(int x) {
-        int a=0;
-        while((long)a*a<=x){
-            a++;
+        return sqrt(x); 
+    }
+    public static int sqrt(int n){
+        double x=n ,tol=0.00001;
+        while(Math.abs(x-n/x)>tol){
+            x=(x+n/x)/2;
         }
-    return a-1;
-    } 
+        return (int) x;
+    }
 }
